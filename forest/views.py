960d1ff13dbaf09c66ad6565e7edf2974d70ae80
root <roots@localhost.localdomain>
2021-05-31 10:26:26 +0300

    Aanchal

--- forest/views.py
+++ forest/views.py
@@ -36,8 +36,7 @@ def forest_movies(request):
         # if user request for hollywood page
         if request.POST.get("hollywood"):
             # changing all movies_post with hollywood related posts
-            query_list = Post.objects.filter(section="hollywood")
-            all_movies_post = reversed(list(query_list))
+            all_movies_post = Post.objects.filter(section="hollywood").order_by("sno").reverse()
             # getting how many pages will built with hollywood content avalable in database
             len_pages = len(all_movies_post)/9
             # parsing a page_id to know "next previous" button's that user is at which page
