92eef6e7a9084088574286a087afe3a52c152b0d
root <roots@localhost.localdomain>
2021-05-31 11:35:25 +0300

    modify

--- forest/views.py
+++ forest/views.py
@@ -158,7 +158,7 @@ def forest_movies(request):
     # making other labels list 
     other = []
     # for loop to filter the labels 
-    for lables_item in labels:
+    for lables_item in labels.iterator():
         if lables_item.categories.isdigit():
             year.append(labels_item)
         else:
