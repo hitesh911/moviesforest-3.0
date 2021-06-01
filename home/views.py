47222f557d74fc373847c49c48e5c7b2bb5423a0
root <roots@localhost.localdomain>
2021-06-01 10:02:34 +0300

    modify

--- home/views.py
+++ home/views.py
@@ -54,11 +54,11 @@ def search(request):
         # passing our query into SeaschQuery postgress function 
         q = SearchQuery(query)
         # making a final search results 
-        search_related_posts = Post.objects.annotate(rank=SearchRank(vector , q , cover_density = True)).filter(rank__gte=0.3).order_by("-rank")
+        search_related_posts = Post.objects.annotate(rank=SearchRank(vector , q , cover_density = True)).filter(rank__gte=1.0).order_by("-rank")
     # checking if search_related_posts are empty after advcance search so search it normally
     if search_related_posts.count() == 0:
         # normal search from database :
-        fetching related posts form database with diffrent colomn's 
+        # fetching related posts form database with diffrent colomn's 
         search_from_title = Post.objects.filter(title__icontains = query)
         search_from_content = Post.objects.filter(content__icontains = query)
         search_from_category = Post.objects.filter(category__icontains = query)
