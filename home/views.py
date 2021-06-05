1bd600349384752cc0a70f8439dd88c54c327061
hitesh911 <hp354998@gmail.com>
2021-06-05 10:36:52 +0300

    modify

--- home/views.py
+++ home/views.py
@@ -83,6 +83,6 @@ def search(request):
 def history(request):
     messages.warning(request , "You history will deleted with out ip change")
     current_user_ip = str(get_client_ip(request = request))
-    history_related_posts = Post.objects.filter(ips__icontains = current_user_ip).order_by("sno").reverse()
+    history_related_posts = Post.objects.filter(ips__icontains = current_user_ip).order_by("section").reverse()
     context = {"history_related_posts":history_related_posts}
     return render(request , "home/history.html" , context)
