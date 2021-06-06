00e18d7969f88d346c13f37ac40ca7f977903fdc
hitesh911 <hp354998@gmail.com>
2021-06-06 20:40:56 +0300

    modfisy searhc

--- home/urls.py
+++ home/urls.py
@@ -4,12 +4,12 @@ from home import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('contact/', views.contact , name='contact'),
-    path('about/', views.about , name='about'),
-    path('login/', views.login , name='login'),
-    path('register/', views.register , name='register'),
-    path('search/', views.search , name='search'),
-    path('history/', views.history , name='history'),
-    path("ZeroTwo/", views.ZeroTwo.as_view(), name="ZeroTwo"),
+    path('contact', views.contact , name='contact'),
+    path('about', views.about , name='about'),
+    path('login', views.login , name='login'),
+    path('register', views.register , name='register'),
+    path('search', views.search , name='search'),
+    path('history', views.history , name='history'),
+    path("ZeroTwo", views.ZeroTwo.as_view(), name="ZeroTwo"),
     
 ]
