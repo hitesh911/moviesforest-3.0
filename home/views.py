9cb7cb79f915e218c184317196d18d767207388e
hitesh911 <hp354998@gmail.com>
2021-06-07 08:08:48 +0300

    zero better

--- home/views.py
+++ home/views.py
@@ -129,7 +129,7 @@ class ZeroTwo(View):
         # making a text to search in zero_two 
         text = f"{movie_name}+{year}"
         # making a perfect url to make first request 
-        url1 = f'{base_url}/api/{API_KEY}/search_movie/{text}/size={size}'
+        url1 = f'{base_url}/api/{API_KEY}/search_movie/{text}/size={size}/bava'
         # making a request to zero_two api 
         try:
             data1 = requests.get(url1).json()
