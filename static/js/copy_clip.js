651f00013d291e99dd59f712f7179f699499c6f4
hitesh911 <hp354998@gmail.com>
2021-06-10 18:35:42 +0300

    modify stream

--- static/js/copy_clip.js
+++ static/js/copy_clip.js
@@ -22,3 +22,81 @@ function copyUrl() {
     document.body.removeChild(dummy);
     alert("Link has been copied Go and share Link..");
 }
+
+
+//grab the video dom element
+const video = document.querySelector('iframe');
+const notifications = document.querySelectorAll('.notification');
+const forwardNotificationValue = document.querySelector('.video-forward-notify span');
+const rewindNotificationValue = document.querySelector('.video-rewind-notify span');
+
+let timer;
+let rewindSpeed = 0;
+let forwardSpeed = 0;
+
+//function for double click event listener on the video
+//todo change those variable to html5 data attributes
+function updateCurrentTime(delta) {
+    let isRewinding = delta < 0;
+
+    if (isRewinding) {
+        rewindSpeed = rewindSpeed + delta;
+        forwardSpeed = 0;
+    } else {
+        forwardSpeed = forwardSpeed + delta;
+        rewindSpeed = 0;
+    }
+
+    //clear the timeout
+    clearTimeout(timer);
+
+    let speed = (isRewinding ? rewindSpeed : forwardSpeed);
+    video.currentTime = video.currentTime + speed;
+
+    let NotificationValue = isRewinding ? rewindNotificationValue : forwardNotificationValue;
+    NotificationValue.innerHTML = `${Math.abs(speed)} seconds`;
+
+    //reset accumulator within 2 seconds of a double click
+    timer = setTimeout(function() {
+        rewindSpeed = 0;
+        forwardSpeed = 0;
+    }, 2000); // you can edit this delay value for the timeout, i have it set for 2 seconds
+    console.log(`updated time: ${video.currentTime}`);
+}
+
+
+function animateNotificationIn(isRewinding) {
+    isRewinding ? notifications[0].classList.add('animate-in') : notifications[1].classList.add('animate-in');
+}
+
+function animateNotificationOut() {
+    this.classList.remove('animate-in');
+}
+
+function forwardVideo() {
+    updateCurrentTime(10);
+    animateNotificationIn(false);
+}
+
+function rewindVideo() {
+    updateCurrentTime(-10);
+    animateNotificationIn(true);
+}
+
+//Event Handlers
+function doubleClickHandler(e) {
+    console.log(`current time: ${video.currentTime}`);
+    const videoWidth = video.offsetWidth;
+    (e.offsetX < videoWidth / 2) ? rewindVideo(): forwardVideo();
+}
+
+function togglePlay() {
+    video.paused ? video.play() : video.pause();
+}
+
+//Event Listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('dblclick', doubleClickHandler);
+notifications.forEach(function(notification) {
+    notification.addEventListener('animationend', animateNotificationOut);
+});
