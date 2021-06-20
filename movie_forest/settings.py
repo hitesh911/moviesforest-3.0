82992298e63671b787f5d707c7f530b97d88b235
hitesh911 <hp354998@gmail.com>
2021-06-20 11:10:34 +0300

    modifY

--- movie_forest/settings.py
+++ movie_forest/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY'] 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG  = False
+DEBUG  = True
 
 ALLOWED_HOSTS = ["moviesforest.herokuapp.com" , "localhost" , "smacker-hacker-1-movies-forest.zeet.app" , "www.themoviesforest.gq", "themoviesforest.gq"]
 
