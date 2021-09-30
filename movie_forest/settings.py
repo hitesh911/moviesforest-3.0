10bbcf2aae2c46cc299b2aac12dba821cb3beb59
hitesh911 <72139864+hitesh911@users.noreply.github.com>
2021-09-30 20:09:53 +0530

    Update settings.py

--- movie_forest/settings.py
+++ movie_forest/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG  = False
 
-ALLOWED_HOSTS = ["moviesforest.herokuapp.com" , "localhost" , "themoviesforest.herokuapp.com" , "www.themoviesforest.gq", "themoviesforest.gq"]
+ALLOWED_HOSTS = ["temporarymovies.herokuapp.com","moviesforest.herokuapp.com" , "localhost" , "themoviesforest.herokuapp.com" , "www.themoviesforest.gq", "themoviesforest.gq"]
 
 
 # Application definition
